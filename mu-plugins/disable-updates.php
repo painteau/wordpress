e2306dc55b2ae7ac3f47d16a9b064141089ad321
KaO <thierryoliv@gmail.com>
2026-07-12 19:30:06 +0200

    feat: mu-plugin disable auto-updates

---
+++ mu-plugins/disable-updates.php
@@ -0,0 +1,6 @@
+<?php
+add_filter('automatic_updater_disabled', '__return_true');
+add_filter('auto_update_core', '__return_false');
+add_filter('auto_update_plugin', '__return_false');
+add_filter('auto_update_theme', '__return_false');
+add_filter('auto_update_translation', '__return_false');
